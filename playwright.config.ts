f02111f0fa0eb360763466bb20bc4e598f38be15
Dasun169 <dasunnavindu2001@gmail.com>
2026-04-11 00:46:59 +0530

    Update configurations on the playwright project - .env file/ reporting structure, projects

---
+++ playwright.config.ts
@@ -0,0 +1,90 @@
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(__dirname, 'configs/.env.stag'),
+  quiet: true,
+});
+
+export default defineConfig({
+  testDir: "./src/tests",
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : 1,
+  timeout: 30_000,
+  expect: {
+    timeout: 10_000,
+  },
+
+  reporter: [
+    ['list'],
+    ['html', {
+      outputFolder: 'reports/playwright-report',
+      open: 'never',
+    }],
+    ['allure-playwright', {
+      resultsDir: 'reports/allure-report/allure-results',
+      suiteTitle: false,
+      detail: true,
+      environmentInfo: {
+        node_version: process.version,
+        platform: process.platform,
+        browsers: process.env.BROWSERS || 'all',
+      }
+    }],
+    [
+      "json",
+      {
+        outputFile: "reports/json-report/results.json",
+      },
+    ],
+  ],
+
+  use: {
+    baseURL: process.env.BASE_URL,
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+      },
+    },
+
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
+
+    // /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
