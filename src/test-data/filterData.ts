23c212bb255a72b2e6d5e69f7306ab1360fb8d55
Dasun169 <dasunnavindu2001@gmail.com>
2026-04-11 03:55:32 +0530

    Store the first element appear for each filtering type and automate process

---
+++ src/test-data/filterData.ts
@@ -0,0 +1,6 @@
+export const filterData = {
+    lowToHighFirstItem: "Sauce Labs Onesie",
+    highToLowFirstItem: "Sauce Labs Fleece Jacket",
+    aToZFirstItem: "Sauce Labs Backpack",
+    zToAFirstItem: "Test.allTheThings() T-Shirt (Red)"
+};
